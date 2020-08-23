c3395d03f7ad6ba025a85a845dc1465db7a25000
rank1Kat <62022374+chauvinhphuoc@users.noreply.github.com>
2020-08-23 19:56:08 +0700

    Update views.py

--- jobs/views.py
+++ jobs/views.py
@@ -14,6 +14,8 @@ def list_job(request):
 
     def extract_time(jobnode):
         return jobnode.find("relative-time", class_="no-wrap").text.strip()
+    
+    Job.objects.all().delete()
 
     page = 1
     while True:
@@ -24,18 +26,14 @@ def list_job(request):
             break
 
         for j in root.find_all("div", class_="Box-row"):
-            job = j.find("div", class_="flex-auto").find("a")
-            path = job.get("href")
-            fullurl = "https://github.com/{}".format(path).strip()
-
-            jobinfo = Job(title=job.text, time=extract_time(j), url=fullurl)
-            jobinfo.save()
+            if j.find("svg", class_="open"):
+                job = j.find("div", class_="flex-auto").find("a")
+                path = job.get("href")
+                fullurl = "https://github.com/{}".format(path).strip()
+                jobinfo = Job(title=job.text, time=extract_time(j), url=fullurl)
+                jobinfo.save()
 
         page = page + 1
-
-    for row in Job.objects.all().reverse():
-        if Job.objects.filter(url=row.url).count() > 1:
-            row.delete()
 
     return render(request, "jobs/list.html", {"jobs": Job.objects.all()})
 
